5e679f6c0912d8f9f8d3e89af6bae409f3fe1e18
Shoesher <raidertheweeb@gmail.com>
2024-10-15 12:35:23 -0400

    working

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -2,14 +2,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.kinematics.Odometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -19,6 +18,8 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.ReplanningConfig;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
 // import com.pathplanner.lib.auto.ReplanningConfig;
 
@@ -41,6 +42,10 @@ public class drivetrain extends TimedRobot {
     // private final Rotation2d rotation2d = new Rotation2d();
     private DifferentialDriveOdometry odometry;
     public AutoBuilder autoBuilder;
+    private DifferentialDriveKinematics kinematics;
+    private final PIDController leftPID;
+    private final PIDController rightPID;
+    private final SimpleMotorFeedforward feedforward;
     
     private drivetrain(){
         leftfront = new WPI_TalonSRX(1);
@@ -52,15 +57,16 @@ public class drivetrain extends TimedRobot {
         leftEncoder = new Encoder(0,2);
         odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(navx.getYaw()), leftEncoder.getDistance(), rightEncoder.getDistance());
         autoBuilder = new AutoBuilder();
+        kinematics = new DifferentialDriveKinematics(1.09);
+        leftPID = new PIDController(1, 0, 0);
+        rightPID = new PIDController(1, 0, 0);
+        feedforward = new SimpleMotorFeedforward(1, 3);
 
         leftfront.setInverted(true);
         leftrear.setInverted(true);
 
         leftrear.follow(leftfront);
         rightrear.follow(rightfront);
-        
-        resetPose();
-
     }
 
     // Methods
@@ -75,9 +81,9 @@ public class drivetrain extends TimedRobot {
             rightfront.set(Leftjoy);
         } else if (Rightjoy > 0.1) {
             leftfront.set(Rightjoy);
-            rightfront.set(-Rightjoy / 1.5);
+            rightfront.set(-Rightjoy);
         } else if (Rightjoy < -0.1) {
-            leftfront.set(-Rightjoy / 1.5);
+            leftfront.set(-Rightjoy);
             rightfront.set(Rightjoy);
         } else {
             Stopdrive();
@@ -85,8 +91,10 @@ public class drivetrain extends TimedRobot {
     
 
         //pathplanner stuff *not working yet :__ (
+            //edit 10/15/2024 I got it working B) -Shahriar
 
-        autoBuilder.configureRamsete(
+        autoBuilder = new AutoBuilder();
+        AutoBuilder.configureRamsete(
             this::getPose, // Robot pose supplier
             this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
             this::getCurrentSpeeds, // Current ChassisSpeeds supplier
@@ -103,43 +111,53 @@ public class drivetrain extends TimedRobot {
               }
               return false;
             },
-            this // Reference to this subsystem to set requirements
-    );}
+            (Subsystem) this // Reference to this subsystem to set requirements
+    );
+    }
+
 
     
     //auto methods:
 
-    //getPosition (pose)
-    public void getPose(){
-        var gyroAngle = navx.getRotation2d();
-        odometry.update(gyroAngle, leftEncoder.getDistance(), rightEncoder.getDistance() ); 
-    }
+    //Gyro Stuff
 
+    //getPosition (pose)
+    public Pose2d getPose(){
+        return odometry.getPoseMeters();
+    }
     //resetPosition (reset pose)
-    public void resetPose() {
+    public void resetPose(Pose2d pose) {
         leftEncoder.reset();
         rightEncoder.reset();
         navx.reset();
-        var gyroAngle = navx.getRotation2d();
-        odometry.update(gyroAngle, leftEncoder.getDistance(), rightEncoder.getDistance() ); 
+        var gyro = Rotation2d.fromDegrees(navx.getYaw());
+
+        odometry.resetPosition(gyro,leftEncoder.getDistance(), rightEncoder.getDistance(), pose);
     }
 
-    //get Chasis speed (speed methods not working because I need to integrate more variables and functions)
 
+    //Kinematics stuff
+
+    //get Chasis speed (getCurrentSpeeds)
     public ChassisSpeeds getCurrentSpeeds() {
-        double leftSpeed = leftEncoder.getRate();
-        double rightSpeed = rightEncoder.getRate();
-        double angularVelocity = Math.toRadians(navx.getRate()); // Get angular velocity from NavX in rad/s
-        return new ChassisSpeeds(leftSpeed, rightSpeed, angularVelocity);
+        return kinematics.toChassisSpeeds(new DifferentialDriveWheelSpeeds((leftEncoder.getRate() * 18.85)/60, rightEncoder.getRate()/60));
     }
-
-    public void drive(double xSpeed, double rot) {
-        var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+    //drive method
+    public void drive(ChassisSpeeds chassisSpeeds) {
+        DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
         setSpeeds(wheelSpeeds);
-      }
-
-
-
+    }
+    //set speeds
+    public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+        final double leftFeedforward = feedforward.calculate(speeds.leftMetersPerSecond);
+        final double rightFeedforward = feedforward.calculate(speeds.rightMetersPerSecond);
+    
+        final double leftOutput = leftPID.calculate(leftEncoder.getRate(), speeds.leftMetersPerSecond);
+        final double rightOutput = rightPID.calculate(rightEncoder.getRate(), speeds.rightMetersPerSecond);
+    
+        leftfront.setVoltage(leftOutput + leftFeedforward);
+        rightfront.setVoltage(rightOutput + rightFeedforward);
+    }
 
     //sends to operator interface
     public static drivetrain getInstance(){
@@ -149,21 +167,3 @@ public class drivetrain extends TimedRobot {
         return drivetrain;
     }
 }
-
-    //Get Chasis speed
-   
-
-    //Drive with current chasis speed
-
-    /*
-     public void drive(chasis speed){
-        Motor.set(chasis speed)
-        Motor.set(chasis speed)
-     }
-
-
-
-     */
-
-
-
